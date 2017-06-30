eceeaacfda966ac83aedb7994b8721ed9e21619c
Wesley Adamo <weslleyadamo@gmail.com>
2017-06-30 16:10:33 -0300

    Atualizando view IMC

--- app/views/phr/imc.blade.php
+++ app/views/phr/imc.blade.php
@@ -2,19 +2,15 @@
 
 @section("content")
 
-	<div data-role="page" data-title="IMC">
+    <div data-role="page" data-title="IMC">
 
-	    <div role="main" class="ui-content jqm-content jqm-fullwidth">
+        <div role="main" class="ui-content jqm-content jqm-fullwidth">
 
             <a id="newrecord" href="#" class="ui-btn ui-corner-all ui-shadow ui-btn-inline ui-icon-plus ui-btn-icon-left ui-btn-a" data-transition="pop">Novo Registro</a>
             <a id="history" href="#" class="ui-btn ui-corner-all ui-shadow ui-btn-inline ui-icon-fa-list-alt ui-btn-icon-left ui-btn-a" data-transition="pop">Histórico</a>
 
              <a id="imc" href="#" class="ui-btn ui-corner-all ui-shadow ui-btn-inline ui-icon-fa-list-alt ui-btn-icon-left ui-btn-a" data-transition="pop">IMC</a>
 
-
-
-             
-              
 
             {{-- Histórico --}}
             <div id="history_div" class="ui-corner-all custom-corners">
@@ -23,17 +19,16 @@
                 </div>
                 <div class="ui-body ui-body-a">
 
-					@if(count($records) > 0)
-						<img src={{URL::to('/grafico?t=3&p='.$pid)}} alt="Glicose" height="600" width="100%" />
-
+                    @if(count($records) > 0)
+                        <img src={{URL::to('/grafico?t=3&p='.$pid)}} alt="Glicose" height="600" width="100%" />
                         <br>
                         <br>
                         <img src={{URL::to('/grafico?t=2&p='.$pid)}} alt="Peso" height="600" width="100%" />
 
-					@endif
-				
-					<br >
-				
+                    @endif
+                
+                    <br >
+                
                     @foreach($records as $r)
 
                        <div id="history_div" class="ui-corner-all custom-corners">
@@ -42,7 +37,8 @@
                             </div>
                             <div class="ui-body ui-body-a">
                                 Altura: <strong> {{$r['height']}} cm. </strong><br>
-                                Peso: <strong> &nbsp;  {{$r['weigth']}} kg. </strong> 
+                                Peso: <strong> &nbsp;  {{$r['weigth']}} kg. </strong>
+
                             </div>
                         </div>
 
@@ -63,14 +59,10 @@
                         <img src={{URL::to('/grafico?t=4&p='.$pid)}} alt="Imc" height="600" width="100%" />
                         <br>
 
-
                     @endif
                 
                     <br >
-
-
-                    
-                
+     
                     @foreach($records as $r)
 
                        <div id="imc_div" class="ui-corner-all custom-corners">
@@ -88,8 +80,6 @@
             </div>
 
 
-
-
             {{-- Novo registro --}}
             <div id="newrecord_div" class="ui-corner-all custom-corners" style="display: none;">
                 
@@ -103,9 +93,13 @@
 
                         
                          <div class="form-group">
-                   
-                            <label for="height">Altura (cm): </label>   
-                            <input  type="number" name="height" id="height" value="" placeholder="exemplo: 180"  required="required">  
+                            <label for="height">Altura (cm): </label>  
+                             
+                             @if(count($records) > 0)
+                                <input  type="number" name="height" id="height" value="{{$records[0]['height']}}" readonly="readonly">  
+                            @else
+                                <input  type="number" name="height" id="height" value="" placeholder="exemplo: 180"  required="required"> 
+                            @endif
 
                             <label for="weight">Peso (kg):</label>   
                             <input type="number" name="weight" id="weight" value="" placeholder="exemplo:80" required="required">
@@ -127,9 +121,9 @@
                
             </div>
 
-		</div><!-- /content -->
+        </div><!-- /content -->
 
-	</div><!-- /page -->
+    </div><!-- /page -->
 
 @stop
 
@@ -161,15 +155,8 @@
 
     $('#save').click(function(){
 
-        if($('#height').val()=="" || $('#weight').val()==""){
-        
-        }
-        else{
-
-         
-        $('#form').submit();
-        }
-        
+        if($('#height').val()=="" || $('#weight').val()==""){ }
+        else $('#form').submit();
         
     });
 
